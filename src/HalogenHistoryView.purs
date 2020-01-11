49e6c568527e94e0b673dc87aae6bff3e8ccb749
kritzcreek <christoph.hegemann1337@gmail.com>
2020-01-11 01:52:07 +0100

    Refactoring and styling

--- src/HalogenHistoryView.purs
+++ src/HalogenHistoryView.purs
@@ -7,15 +7,19 @@ import Data.Array as Array
 import Data.Const (Const(..))
 import Data.Foldable (fold, foldl)
 import Data.Identity (Identity(..))
-import Data.Maybe (Maybe(..), fromMaybe, isJust, isNothing, maybe)
+import Data.Maybe (Maybe(..), isJust, isNothing, maybe)
 import Data.Newtype (un)
-import Debug.Trace (spy, traceM)
+import Data.String as String
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 
-type PersonR f = ( birthday :: f String, name :: f String, rating :: f Int )
+type PersonR f =
+  ( birthday :: f String
+  , name :: f String
+  , rating :: f Int
+  )
 
 type Person = Record (PersonR Identity)
 type PersonUpdate = Record (date :: String | PersonR Maybe)
@@ -46,10 +50,11 @@ isFiltered filters update =
            || (filterRating && isJust update.rating))
 
 initialPerson :: Person
-initialPerson = { birthday: Identity "01.01.1970", name: Identity "Carl", rating: Identity 1 }
-
-emptyUpdate :: String -> PersonUpdate
-emptyUpdate date = { date, birthday: Nothing, name: Nothing, rating: Nothing }
+initialPerson =
+  { birthday: Identity "01.01.1970"
+  , name: Identity "Carl"
+  , rating: Identity 1
+  }
 
 updates :: Array PersonUpdate
 updates =
@@ -58,13 +63,16 @@ updates =
   , (emptyUpdate "05.01.2020") { birthday = Just "20.02.1992" }
   , (emptyUpdate "03.01.2020") { name = Just "Carlotta" }
   ]
+  where
+    emptyUpdate date =
+      { date, birthday: Nothing, name: Nothing, rating: Nothing }
 
 type Html m = H.ComponentHTML Action () m
 
 type State =
   { initial :: Person
   , updates :: Array PersonUpdate
-  , hovered :: Maybe Int
+  , hoveredRow :: Maybe Int
   , filters :: Filters
   }
 
@@ -85,7 +93,7 @@ initialState :: forall i. i -> State
 initialState _ =
   { initial: initialPerson
   , updates
-  , hovered: Nothing
+  , hoveredRow: Nothing
   , filters:
       { name: Const false
       , birthday: Const false
@@ -102,112 +110,100 @@ render state =
 table :: forall m. State -> H.ComponentHTML Action () m
 table state =
   HH.table
-    [ HP.class_ (HH.ClassName "hhv-table")
-    , HE.onMouseLeave (\_ -> Just LeaveTable)
+    [ HE.onMouseLeave (\_ -> Just LeaveTable) ] $
+    [ tableHeader state
+    , personRow "Current" (applyUpdates state.initial state.updates)
     ]
-    ([tableHeader state, personRow "Current" (applyUpdates state.initial state.updates)]
-      <> Array.mapWithIndex updateRow state.updates
-      <> [personRow "Initial" state.initial])
+    <> Array.mapWithIndex updateRow state.updates
+    <> [personRow "Initial" state.initial]
   where
     updateRow :: Int -> PersonUpdate -> Html m
     updateRow ix update =
-      let active = Just ix == state.hovered in
-      let userInTime = applyUpdates state.initial (Array.takeEnd ((Array.length state.updates) - ix) state.updates) in
-      let filtered = isFiltered state.filters update in
+      let
+        active = Just ix == state.hoveredRow
+        userInTime =
+          applyUpdates
+            state.initial
+            (Array.takeEnd
+              (Array.length state.updates - ix)
+              state.updates)
+        filtered = isFiltered state.filters update
+        mkCell :: forall a. (a -> String) -> Identity a -> Maybe a -> Html m
+        mkCell display shadowVal val =
+          HH.td
+            [ HP.classes $ fold
+                [ pure (HH.ClassName "TableElement")
+                , guard (isNothing val) $> HH.ClassName "TableElement--shadow"
+                ]
+            ]
+            [ HH.text
+                if active
+                then display (un Identity shadowVal)
+                else maybe "" display val
+            ] in
       HH.tr
           [ HP.classes $ fold
-              [ pure (HH.ClassName "hhv-TableRow")
-              , guard filtered $> HH.ClassName "hhv-TableRow--filtered"
+              [ pure (HH.ClassName "TableRow")
+              , guard filtered $> HH.ClassName "TableRow--filtered"
               ]
           , HE.onMouseEnter (\_ -> Just (HoverRow ix))
+          , HE.onMouseLeave (\_ -> Just LeaveTable)
           ]
           [ HH.td
-              [ HP.class_ (HH.ClassName "hhv-TableElement") ]
+              [ HP.class_ (HH.ClassName "TableElement") ]
               [ HH.text update.date ]
-          , HH.td
-              [ HP.classes $ fold
-                [ pure (HH.ClassName "hhv-TableElement")
-                , guard (isNothing update.name) $> HH.ClassName "hhv-TableElement--shadow"
-                ]
-              ]
-              [ HH.text if active then un Identity userInTime.name else fromMaybe "" update.name ]
-          , HH.td
-              [ HP.classes $ fold
-                [ pure (HH.ClassName "hhv-TableElement")
-                , guard (isNothing update.birthday) $> HH.ClassName "hhv-TableElement--shadow"
-                ]
-              ]
-              [ HH.text if active then un Identity userInTime.birthday else fromMaybe "" update.birthday ]
-          , HH.td
-              [ HP.classes $ fold
-                [ pure (HH.ClassName "hhv-TableElement")
-                , guard (isNothing update.rating) $> HH.ClassName "hhv-TableElement--shadow"
-                ]
-              ]
-              [ HH.text if active then show (un Identity userInTime.rating) else maybe "" show update.rating ]
+          , mkCell identity userInTime.name update.name
+          , mkCell identity userInTime.birthday update.birthday
+          , mkCell show userInTime.rating update.rating
           ]
 
 personRow :: forall m. String -> Person -> Html m
 personRow label person =
   HH.tr
-      [ HP.class_ (HH.ClassName "hhv-TableRow") ]
-      [ HH.td
-        [ HP.class_ (HH.ClassName "hhv-TableElement") ]
-        [ HH.text label ]
-      , HH.td
-        [ HP.class_ (HH.ClassName "hhv-TableElement") ]
-        [ HH.text (un Identity person.name) ]
-      , HH.td
-          [ HP.class_ (HH.ClassName "hhv-TableElement") ]
-          [ HH.text (un Identity person.birthday) ]
-      , HH.td
-          [ HP.class_ (HH.ClassName "hhv-TableElement") ]
-          [ HH.text (show (un Identity person.rating)) ]
+      [ HP.class_ (HH.ClassName "TableRow") ]
+      [ mkCell identity (Identity label)
+      , mkCell identity person.name
+      , mkCell identity person.birthday
+      , mkCell show person.rating
       ]
+  where
+    mkCell :: forall a. (a -> String) -> Identity a -> Html m
+    mkCell display val =
+      HH.td
+        [ HP.class_ (HH.ClassName "TableElement") ]
+        [ HH.text (display (un Identity val)) ]
 
 tableHeader :: forall m. State -> Html m
 tableHeader state =
   HH.thead_
     [ HH.tr_
         [ HH.th
-          [ HP.class_ (H.ClassName "hhv-TableHeader") ]
-          [ HH.text "" ]
-        , HH.th
-            [ HP.classes $ fold
-                [ pure (H.ClassName "hhv-TableHeader")
-                , guard (un Const state.filters.name) $> H.ClassName "hhv-TableHeader--filtered"
-                ]
-            , HE.onClick (\_ -> Just (ToggleFilter "name"))
-            ]
-            [ HH.text "Name" ]
-        , HH.th
-            [ HP.classes $ fold
-                [ pure (H.ClassName "hhv-TableHeader")
-                , guard (un Const state.filters.birthday) $> H.ClassName "hhv-TableHeader--filtered"
-                ]
-            , HE.onClick (\_ -> Just (ToggleFilter "birthday"))
-            ]
-            [ HH.text "Birthday" ]
-        , HH.th
-            [ HP.classes $ fold
-                [ pure (H.ClassName "hhv-TableHeader")
-                , guard (un Const state.filters.rating) $> H.ClassName "hhv-TableHeader--filtered"
-                ]
-            , HE.onClick (\_ -> Just (ToggleFilter "rating"))
-            ]
-            [ HH.text "Rating" ]
+            [ HP.class_ (H.ClassName "TableHeader") ]
+            [ HH.text "" ]
+        , mkCell state.filters.name "Name"
+        , mkCell state.filters.birthday "Birthday"
+        , mkCell state.filters.rating "Rating"
         ]
-  ]
+    ]
+  where
+    mkCell :: forall a. Const Boolean a -> String -> Html m
+    mkCell filtered label =
+      HH.th
+        [ HP.classes $ fold
+            [ pure (H.ClassName "TableHeader")
+            , guard (un Const filtered) $> H.ClassName "TableHeader--filtered"
+            ]
+        , HE.onClick (\_ -> Just (ToggleFilter (String.toLower label)))
+        ]
+        [ HH.text label ]
 
 handleAction ∷ forall o m. Action → H.HalogenM State Action () o m Unit
 handleAction = case _ of
-  HoverRow ix -> do
-    { hovered } <- H.get
-    unless (hovered == Just ix) do
-      H.modify_ (_ { hovered = Just ix })
-  LeaveTable -> do
-    H.modify_ (_ { hovered = Nothing })
-  ToggleFilter lbl -> do
+  HoverRow ix ->
+    H.modify_ (_ { hoveredRow = Just ix })
+  LeaveTable ->
+    H.modify_ (_ { hoveredRow = Nothing })
+  ToggleFilter lbl ->
     H.modify_
       (\prev ->
         prev { filters = case lbl of
